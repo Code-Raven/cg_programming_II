2bb91d117c7bcb90b4f205d342f3dcf2c8e97685
Branden Oden <ODEN@Brandens-MacBook-Pro.local>
2015-08-02 15:28:09 -0700

    added bunny model

--- src/World.h
+++ src/World.h
@@ -4,15 +4,6 @@
 
 #pragma once
 
-#define ASCII_ZERO 48
-
-#define LOAD_TEXTURE(chr) (chr == 35)
-#define LOAD_LEVELDATA(chr) (chr == 40)
-#define IS_NUMBER(chr) (chr > 47 && chr < 58)
-#define IS_EMPTY_LINE(chr) (chr == 32 || chr == '\0')
-
-#define LEVEL_0 "level_0.txt"
-
 class World{
 
 	public:
